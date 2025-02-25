14829fcd145b4a096170d82d5ebb0b9d74b48b93
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2025-02-25 15:41:59 -0300

    Operadores

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import static java.lang.System.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello World and Welcome Davi Favaro!");
+        out.printf("Hello World and Welcome Davi Favaro!");
+        out.println(9 + 2);
+        out.println(9 - 2);
+        out.println(9 * 2);
+        out.println(9 / 2);
     }
 }
