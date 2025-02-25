f851e6f87c756c19e12f592efd3f9ceb1c95ebf6
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2025-02-25 17:22:02 -0300

    OperadoresRelacionais&Com Atribuicoes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # HelloJava
+# (nota1 + nota2) /2 "vai calcular primeiro o que está dentro de '()' e depois o que está por fora."
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
+import java.util.Scanner;
+
 import static java.lang.System.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        out.printf("Hello World and Welcome Davi Favaro!");
-        out.println(9 + 2);
-        out.println(9 - 2);
-        out.println(9 * 2);
-        out.println(9 / 2);
+
+        double nota1 = 9.0;
+        double nota2 = 10.0;
+
+        double media;
+        media = (nota1 + nota2) /2;
+        out.println(media);
+
+        int valor;
+        Scanner teclado = new Scanner(in);
+        out.println("Digite um valor: ");
+        valor = teclado.nextInt();
+
+        out.println(valor/2);
+        out.println(valor%2);
+
+        int num1, num2;
+        num1 = 6;
+        num2 = 6;
+
+        out.println(num1);
+        out.println(num2);
+
+        num1++; // num1 = num1 + 1;
+        ++num2; // num2 = num2 + 1
+
+        out.println(num1);
+        out.println(num2);
+
+        int numero;
+        numero = 6;
+        numero -= 2;
+        out.println(numero);
+
+        out.println(numero >= 2); // maior ou igual
+        out.println(numero == 7); // igual
+        out.println(numero != 7); // diferente
     }
 }
